4c0a7f41afbf4674c5d820cc21cf5307ca769dd3
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2022-08-22 22:52:19 -0600

    Yeah, yeah, I don't wanna talk about it.

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -16,17 +16,17 @@ type assertion struct {
 	actual interface{}
 }
 
-func (this *assertion) IsNil()   { this.t.Helper(); this.Equals(nil) }
-func (this *assertion) IsTrue()  { this.t.Helper(); this.Equals(true) }
-func (this *assertion) IsFalse() { this.t.Helper(); this.Equals(false) }
-func (this *assertion) Equals(expected interface{}) {
-	this.t.Helper()
-	if !reflect.DeepEqual(this.actual, expected) {
-		this.t.Errorf("\n"+
+func (a *assertion) IsNil()   { a.t.Helper(); a.Equals(nil) }
+func (a *assertion) IsTrue()  { a.t.Helper(); a.Equals(true) }
+func (a *assertion) IsFalse() { a.t.Helper(); a.Equals(false) }
+func (a *assertion) Equals(expected interface{}) {
+	a.t.Helper()
+	if !reflect.DeepEqual(a.actual, expected) {
+		a.t.Errorf("\n"+
 			"Expected: %#v\n"+
 			"Actual:   %#v",
 			expected,
-			this.actual,
+			a.actual,
 		)
 	}
 }
